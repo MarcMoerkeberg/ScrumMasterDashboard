bdc87ee4deb81e5ef5d80e0c8d942ec0d501c082
MC <mch@e-komplet.dk>
2024-01-20 13:04:22 +0100

    Add db context models

---
+++ ScrumMasterDashboard.Api/DAL/Context/DefaultContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScrumMasterDashboard.Api.DAL.Entities;
+
+namespace ScrumMasterDashboard.Api.DAL.Context
+{
+	public class DefaultContext : DbContext
+	{
+		public DbSet<TeamMember> TeamMembers { get; set; }
+		public DefaultContext(DbContextOptions contextOptions) : base(contextOptions)
+		{
+		}
+	}
+}
