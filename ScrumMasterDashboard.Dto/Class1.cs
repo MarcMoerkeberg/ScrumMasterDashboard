0b43651cf2c237f23539dda603738aaec75fbc53
MarcMoerkeberg <mc.christiansen@outlook.com>
2024-02-25 14:40:36 +0100

    Add TeamMemberDTO

--- ScrumMasterDashboard.Dto/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ScrumMasterDashboard.Dto
-{
-	public class Class1
-	{
-
-	}
-}
---
+++ ScrumMasterDashboard.Dto/TeamMemberDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScrumMasterDashboard.Dto
+{
+	public class TeamMemberDTO
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[MaxLength(100)]
+		[MinLength(1)]
+		public string Name { get; set; } = string.Empty;
+	}
+}
