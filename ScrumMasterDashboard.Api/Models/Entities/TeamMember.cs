a9e768cd0a150cb86feb4abd493dace51167a260
MarcMoerkeberg <mc.christiansen@outlook.com>
2024-03-17 09:39:29 +0100

    Update properties on model and only make Id set-able on init of object

--- ScrumMasterDashboard.Api/Models/Entities/TeamMember.cs
+++ ScrumMasterDashboard.Api/Models/Entities/TeamMember.cs
@@ -3,15 +3,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ScrumMasterDashboard.Api.Models.Entities
 {
-    [Table("TeamMembers")]
-    public class TeamMember
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+	[Table("TeamMembers")]
+	public class TeamMember
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; init; }
 
-        [Required]
-        [MaxLength(100)]
-        public string Name { get; set; } = string.Empty;
-    }
+		[Required]
+		[MaxLength(100)]
+		public string Name { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Alters the properties with those on <paramref name="updatedTeamMember"/>.<br/>
+		/// It returns the updated <see cref="TeamMember"/> to allow for method chaining.
+		/// </summary>
+		/// <param name="updatedTeamMember">Should contain the properties to update the target <see cref="TeamMember"/>.</param>
+		public TeamMember UpdateProperties(TeamMember updatedTeamMember)
+		{
+			Name = updatedTeamMember.Name;
+
+			return this;
+		}
+	}
 }
